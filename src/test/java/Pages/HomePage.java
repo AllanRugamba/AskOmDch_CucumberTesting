35bcbda806621393bdeb132a4b7b9377e9c4039b
Allan-lang <allan.rugamba2023@kepler.org>
2026-01-20 11:52:56 +0200

    Add all step definitions and page objects for complete test automation framework

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -10,7 +10,6 @@ public class HomePage {
     private String url = "https://askomdch.com/";
     private By blueShoesProduct = By.xpath("//h2[contains(@class, 'woocommerce-loop-product__title') and normalize-space()='Blue Shoes']");
 
-
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
@@ -18,6 +17,7 @@ public class HomePage {
     public void open() {
         driver.get(url);
     }
+
     public boolean isBlueShoesProductVisible(){
         List<WebElement> blueShoesProducts = driver.findElements(blueShoesProduct);
         return !blueShoesProducts.isEmpty();
@@ -28,20 +28,19 @@ public class HomePage {
     }
 
     public void addProductToCart(String productName) {
-        // Find the product card containing the given name
         String productXpath = "//h2[contains(@class,'woocommerce-loop-product__title') and normalize-space()='" 
                 + productName + "']/ancestor::li//a[contains(@class,'add_to_cart_button')]";
 
         WebElement addToCartButton = driver.findElement(By.xpath(productXpath));
         addToCartButton.click();
         
-        // Wait for the add to cart action to complete
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
     }
+
     public String getProductName() {
         if (isBlueShoesProductVisible()) {
             return driver.findElement(blueShoesProduct).getText();
@@ -50,23 +49,9 @@ public class HomePage {
     }
 
     public void openCart() {
-        // Try multiple ways to access cart
-        try {
-            driver.findElement(By.linkText("Cart")).click();
-        } catch (Exception e) {
-            try {
-                driver.findElement(By.partialLinkText("Cart")).click();
-            } catch (Exception e2) {
-                try {
-                    driver.findElement(By.cssSelector("a[href*='cart']")).click();
-                } catch (Exception e3) {
-                    // Navigate directly to cart URL
-                    driver.get(url + "cart/");
-                }
-            }
-        }
+        // Use the specific cart icon selector
+        driver.findElement(By.cssSelector("#ast-site-header-cart > div.ast-site-header-cart-li.current-menu-item > a > div")).click();
         
-        // Wait for cart page to load
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
